6666f313aeb7831578a761787d29cb1f91d3a547
ksmart1 <ksmart1@ksmart.or.kr>
2021-12-28 10:25:18 +0900

    [team1] 초기 프로젝트 셋팅 files:파일 이름 details: 간략한 설명

---
+++ teamproject_layout/src/main/java/ksteam/project/TeamprojectLayoutApplication.java
@@ -0,0 +1,13 @@
+package ksteam.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamprojectLayoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamprojectLayoutApplication.class, args);
+	}
+
+}
---
+++ teamproject_layout/src/test/java/ksteam/project/TeamprojectLayoutApplicationTests.java
@@ -0,0 +1,13 @@
+package ksteam.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamprojectLayoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
